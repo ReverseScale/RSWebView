ed93bf1089ee392332441c11aa498b1e9d26b3a5
XieTian <Stevens@Stevens-MacBook-Air.local>
2017-11-17 17:35:31 +0800

    Update Code

---
+++ RSWebView/TableViewController.swift
@@ -0,0 +1,71 @@
+//
+//  TableViewController.swift
+//  RSWebView
+//
+//  Created by WhatsXie on 2017/11/17.
+//  Copyright © 2017年 R.S. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0:
+            pushWebView()
+        case 1:
+            pushAppStore()
+        case 2:
+            modelWebView()
+        default:
+            modelWebViewDark()
+        }
+    }
+
+    
+    func pushWebView() {
+        let webVC = SwiftWebVC(urlString: "https://www.baidu.com")
+        webVC.delegate = self
+        self.navigationController?.pushViewController(webVC, animated: true)
+    }
+    func pushAppStore() {
+        let webVC = SwiftWebVC(urlString: "https://itunes.apple.com/us/app/habitminder/id1253577148?mt=8")
+        webVC.delegate = self
+        self.navigationController?.pushViewController(webVC, animated: true)
+    }
+    func modelWebView() {
+        let webVC = SwiftModalWebVC(urlString: "https://www.baidu.com", theme: .lightBlack, dismissButtonStyle: .cross)
+        self.present(webVC, animated: true, completion: nil)
+    }
+    func modelWebViewDark() {
+        let webVC = SwiftModalWebVC(urlString: "https://www.baidu.com", theme: .dark, dismissButtonStyle: .arrow)
+        self.present(webVC, animated: true, completion: nil)
+    }
+}
+
+extension TableViewController: SwiftWebVCDelegate {
+    func didStartLoading() {
+        print("Started loading.")
+    }
+    
+    func didFinishLoading(success: Bool) {
+        print("Finished loading. Success: \(success).")
+    }
+}
